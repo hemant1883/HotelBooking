c957340a3cd3bd99a756e592be4de50a0ad7dfed
hemant1883 <167603000+hemant1883@users.noreply.github.com>
2026-08-14 10:45:04 +0530

    Update HotelController.java

--- src/main/java/com/example/HotelManagement/controller/HotelController.java
+++ src/main/java/com/example/HotelManagement/controller/HotelController.java
@@ -18,6 +18,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/hotels")
 @RequiredArgsConstructor
+    @CrossOrigin(origins = {
+    "http://localhost:5173",
+    "https://hotel-booking-gamma-liart.vercel.app"
+})
 public class HotelController {
 
     private final HotelService hotelService;
